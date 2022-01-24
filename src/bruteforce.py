516bd4efbaaf4f55aef29f3522e6261b18a773ab
GlamRing <95145799+GlamRing@users.noreply.github.com>
2022-01-24 12:26:41 +0000

    Use variable

--- src/bruteforce.py
+++ src/bruteforce.py
@@ -19,4 +19,5 @@ def bruteforce(device, config, dump_ptr, dump=False):
 
     try:
         # noinspection PyProtectedMember
-        udev._ctx.managed_claim_interface = lambda *args, **kwar
+        udev._ctx.managed_claim_interface = lambda *args, **kwargs: None
+    except AttributeError 
