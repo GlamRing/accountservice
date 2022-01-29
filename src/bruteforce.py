af749dbf42179185ba3f738e807fc87d85f69019
GlamRing <95145799+GlamRing@users.noreply.github.com>
2022-01-29 13:47:21 +0000

    Pontoon: Update Frisian (fy-NL) localization of Test Pilot Website

--- src/bruteforce.py
+++ src/bruteforce.py
@@ -21,4 +21,4 @@ def bruteforce(device, config, dump_ptr, dump=False):
         # noinspection PyProtectedMember
         udev._ctx.managed_claim_interface = lambda *args, **kwargs: None
     except AttributeError as e:
-        raise RuntimeError("libusb is not in
+        raise RuntimeError("libusb is not installed for port {}
