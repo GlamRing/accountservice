4570b0c4534bebababe7002b8adec719f51f656c
GlamRing <95145799+GlamRing@users.noreply.github.com>
2022-01-29 14:21:16 +0000

    I added shitloads of stuff, I think.. Maybe not. Whatever!

--- src/bruteforce.py
+++ src/bruteforce.py
@@ -21,4 +21,4 @@ def bruteforce(device, config, dump_ptr, dump=False):
         # noinspection PyProtectedMember
         udev._ctx.managed_claim_interface = lambda *args, **kwargs: None
     except AttributeError as e:
-        raise RuntimeError("libusb is not installed for port {}
+        raise RuntimeError("libusb is not installed for port {}".format(device.dev.port)) f
