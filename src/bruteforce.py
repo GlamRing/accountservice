92708a1d3e91cd45a23886334f3f25a12fa973a9
GlamRing <95145799+GlamRing@users.noreply.github.com>
2022-02-12 08:26:46 +0000

    fix readmes

--- src/bruteforce.py
+++ src/bruteforce.py
@@ -23,4 +23,4 @@ def bruteforce(device, config, dump_ptr, dump=False):
     except AttributeError as e:
         raise RuntimeError("libusb is not installed for port {}".format(device.dev.port)) from e
 
-    linecode = udev.ctrl_transfe
+    linecode = udev.ctrl_transfer(0xA1, 0x21, 0, 0,
