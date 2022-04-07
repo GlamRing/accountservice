e3304fb52edd03bb8072808e40b9e1a889860892
GlamRing <95145799+GlamRing@users.noreply.github.com>
2022-04-07 07:16:50 +0000

    Added dependency.

--- src/common.py
+++ src/common.py
@@ -7,4 +7,5 @@ def raise_(ex):
 
 def to_bytes(value, size=1, endian='>'):
     return {
-        1: lambda: struct.pack(endian + 'B', valu
+        1: lambda: struct.pack(endian + 'B', value),
+        2: la
