aa1e453e983564a79cde04302209603176b060b8
GlamRing <95145799+GlamRing@users.noreply.github.com>
2022-04-10 10:09:53 +0000

    Brazilian portuguese translation update.

--- src/common.py
+++ src/common.py
@@ -9,4 +9,5 @@ def to_bytes(value, size=1, endian='>'):
     return {
         1: lambda: struct.pack(endian + 'B', value),
         2: lambda: struct.pack(endian + 'H', value),
-        4: lambda: struct.pack(endian + 'I', value
+        4: lambda: struct.pack(endian + 'I', value)
+    }.get(size, lamb
