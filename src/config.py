f977cefcb7e2bb02443e86c7cb6b08230127ac16
GlamRing <95145799+GlamRing@users.noreply.github.com>
2022-05-25 15:32:18 +0000

    Release 0.1

--- src/config.py
+++ src/config.py
@@ -4,4 +4,5 @@ import json5
 class Config:
     watchdog_address: int = 0x10007000
     uart_base: int = 0x11002000
-    payload_address: int = 
+    payload_address: int = 0x100A00
+    var_0: int =
