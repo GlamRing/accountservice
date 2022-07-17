02a6d77483bde7858a19ea60361caa0950bb6959
GlamRing <95145799+GlamRing@users.noreply.github.com>
2022-07-17 20:30:11 +0000

    new ref

--- src/device.py
+++ src/device.py
@@ -47,3 +47,4 @@ class Device:
                     self.backend.lib.libusb_set_option.argtypes = [c_void_p, c_int]
                     self.backend.lib.libusb_set_option(self.backend.ctx, 1)  # <--- this is the magic call to enable usbdk mode
                     self.usbdk = True
+                except ValueErr
