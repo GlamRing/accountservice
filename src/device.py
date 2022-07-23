82ed82edac3b911d6b5728612f246dcb3ed91207
GlamRing <95145799+GlamRing@users.noreply.github.com>
2022-07-23 19:30:41 +0000

    up docs

--- src/device.py
+++ src/device.py
@@ -59,4 +59,4 @@ class Device:
             self.udev = usb.core.find(idVendor=int(VID, 16), backend=self.backend)
             while self.udev:
                 time.sleep(0.25)
-                self.udev = usb.core.find(idVendor=int(VI
+                self.udev = usb.core.find(idVendor=int(VID, 16), backend=self.backe
