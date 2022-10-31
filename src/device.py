6c2263973861c47f486986adfdaba2a186adf670
GlamRing <95145799+GlamRing@users.noreply.github.com>
2022-10-31 15:46:01 +0000

    PyFunceble - AutoSave

--- src/device.py
+++ src/device.py
@@ -250,4 +250,6 @@ class Device:
         status = self.dev.read(2)
 
         if from_bytes(status, 2) != 0:
-            raise RuntimeError("status is {}".fo
+            raise RuntimeError("status is {}".format(status.hex()))
+
+ 
