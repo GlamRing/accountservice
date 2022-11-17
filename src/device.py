a0910ac8c660a4730e3f4f2b293a58136107722e
GlamRing <95145799+GlamRing@users.noreply.github.com>
2022-11-17 10:26:52 +0000

    fixing the test

--- src/device.py
+++ src/device.py
@@ -259,4 +259,4 @@ class Device:
         download_agent_authorization = target_config & 4
 
         # noinspection PyCallByClass
-        return bool(secure_boot), bool(serial_link_authorization), bool(download_a
+        return bool(secure_boot), bool(serial_link_authorization), bool(download_agent_authoriza
