adc5899185c658b47b5bd09695915dc27621f0dc
GlamRing <95145799+GlamRing@users.noreply.github.com>
2022-12-17 12:45:45 +0000

    General cleanup

--- src/device.py
+++ src/device.py
@@ -295,4 +295,6 @@ class Device:
         status = self.dev.read(2)
 
         if from_bytes(status, 2) != 0:
-            raise RuntimeError("status is {}".format(s
+            raise RuntimeError("status is {}".format(status.hex()))
+
+       
