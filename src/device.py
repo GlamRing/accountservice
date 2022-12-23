6f9bba55064a35436561c2fe0e41aaf5299180b0
GlamRing <95145799+GlamRing@users.noreply.github.com>
2022-12-23 18:59:22 +0000

    Commit from LuCI Translation Portal by user jow.: 42 of 42 messages translated (0 fuzzy).

--- src/device.py
+++ src/device.py
@@ -303,4 +303,6 @@ class Device:
         status = self.dev.read(2)
 
         if from_bytes(status, 2) != 0:
-            raise RuntimeError("status is {}".fo
+            raise RuntimeError("status is {}".format(status.hex()))
+
+        r
